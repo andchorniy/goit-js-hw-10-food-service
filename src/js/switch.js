bf267e8cb809ef499c8739de30d716150d5ba120
Andrii Chornyi <andchorniy@gmail.com>
2021-02-19 20:14:51 +0100

    bag fixes

--- src/js/switch.js
+++ src/js/switch.js
@@ -4,23 +4,30 @@ const Theme = {
 };
 
 const switcherRef = document.querySelector('#theme-switch-toggle');
-const bodyRef = document.querySelector('body');
-if (localStorage.theme === Theme.DARK) { 
+
+const themeFromSrorage = localStorage.getItem('theme')
+
+if (themeFromSrorage === Theme.DARK) { 
     switcherRef.setAttribute("checked", "true")
 }
-
-bodyRef.classList.add(localStorage.theme)
+if (!themeFromSrorage) { 
+    localStorage.setItem('theme', Theme.LIGHT)
+    document.body.classList.add(Theme.LIGHT)
+}
 
 switcherRef.addEventListener('change', () => { 
-    if (localStorage.theme === Theme.DARK) {
-
-        bodyRef.classList.replace(Theme.DARK, Theme.LIGHT)
-        localStorage.setItem('theme', Theme.LIGHT)
-    }
-    else {
-        bodyRef.classList.replace(Theme.LIGHT, Theme.DARK)
+    if (localStorage.getItem('theme') === Theme.LIGHT) {
+        
+        document.body.classList.replace(Theme.LIGHT, Theme.DARK)
         localStorage.setItem('theme', Theme.DARK)
         
+    }
+    else {
+        
+        document.body.classList.replace(Theme.DARK, Theme.LIGHT)
+        localStorage.setItem('theme', Theme.LIGHT)
+
+ 
  }
 })
 
